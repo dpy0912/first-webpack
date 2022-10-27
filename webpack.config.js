4aabf08801dde0398305d6b40e20c77e47124fe2
杜培义 <peiyi.du@zknow.com>
2022-10-27 17:13:33 +0800

    [add]webpack-dev-middleware发送server：可以作为中间件进行处理，比如一些跨域代理

--- webpack.config.js
+++ webpack.config.js
@@ -8,101 +8,101 @@ const json5 = require('json5');
 
 // 打包的进度条
 const progressPlugin = new WebpackBar({
-	color: '#e59e1c', // 默认green，进度条颜色支持HEX
-	basic: false, // 默认true，启用一个简单的日志报告器
-	profile: false, // 默认false，启用探查器。
+    color: '#e59e1c', // 默认green，进度条颜色支持HEX
+    basic: false, // 默认true，启用一个简单的日志报告器
+    profile: false, // 默认false，启用探查器。
 });
 
-module.exports = function (env, argv) {
-	return {
-		// 开发中模式
-		mode: env.production ? 'production' : 'development',
-		target: 'web', // 打包之后的运行， node和web
-		// 入口文件
-		entry: {
-			index: './src/index.js',
-			print: './src/print.js'
-		},
-		// 出口文件
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			filename: '[name].bundle.js', // name占位符来确保名字是唯一的
-			clean: true, // 每次build之前都出执行清除输出文件的操作
-		},
-		// 使用loader进行类型的转换（test是需要编译的文件类型，use是表示需要使用某种loader进行转换）
-		// 工具库
-		devtool: env.production ? null : 'inline-source-map',
-		// 服务启动（启动一个静态的本地服务）
-		devServer: {
-			// 静态目录
-			static: './dist',
-			// 模块热更新
-			hot: true,
-		},
-		// 优化
-		optimization: {
-			runtimeChunk: 'single'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader'],
-				},
-				{
-					test: /\.ts$/,
-					use: 'ts-loader',
-				},
-				// {
-				// 	test: /\.(js|jsx)$/,
-				// 	use: 'babel-loader',
-				// },
-				// 加载图片
-				{
-					test: /\.(png|svg|jpg|jpeg|gif)$/i,
-					type: 'asset/resource',
-				},
-				{
-					test: /\.(woff|woff2|eot|ttf|otf)$/i,
-					type: 'asset/resource'
-				},
-				// cvs格式
-				{
-					test: /\.(csv|tsv)$/i,
-					use: ['csv-loader'],
-				},
-				// xml格式
-				{
-					test: /\.xml$/i,
-					use: ['xml-loader'],
-				},
-				{
-					test: /\.toml$/i,
-					type: "json",
-					parser: {
-						parse: toml.parse,
-					}
-				},
-				{
-					test: /\.yaml$/i,
-					type: "json",
-					parser: {
-						parse: yaml.parse,
-					}
-				},
-				{
-					test: /\.json5$/i,
-					type: "json",
-					parser: {
-						parse: json5.parse,
-					}
-				}
-			],
-		},
-		// ProgressPlugin进度插件，自定义进度条
-		plugins: [
-			progressPlugin,
-			new HtmlWebpackPlugin({ template: './public/index.html', title: "first webpack"}),
-		],
-	};
+module.exports = {
+    // 开发中模式
+    mode: process.env.production ? 'production' : 'development',
+    target: 'web', // 打包之后的运行， node和web
+    // 入口文件
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
+    // 出口文件
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js', // name占位符来确保名字是唯一的
+        clean: true, // 每次build之前都出执行清除输出文件的操作
+        // 公共的服务目录
+        publicPath: "/",
+    },
+    // 使用loader进行类型的转换（test是需要编译的文件类型，use是表示需要使用某种loader进行转换）
+    // 工具库
+    devtool: process.env.production ? null : 'inline-source-map',
+    // 服务启动（启动一个静态的本地服务）
+    devServer: {
+        // 静态目录
+        static: './dist',
+        // 模块热更新
+        hot: true,
+    },
+    // 优化
+    optimization: {
+        runtimeChunk: 'single'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+            // {
+            // 	test: /\.(js|jsx)$/,
+            // 	use: 'babel-loader',
+            // },
+            // 加载图片
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            },
+            // cvs格式
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            // xml格式
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.toml$/i,
+                type: "json",
+                parser: {
+                    parse: toml.parse,
+                }
+            },
+            {
+                test: /\.yaml$/i,
+                type: "json",
+                parser: {
+                    parse: yaml.parse,
+                }
+            },
+            {
+                test: /\.json5$/i,
+                type: "json",
+                parser: {
+                    parse: json5.parse,
+                }
+            }
+        ],
+    },
+    // ProgressPlugin进度插件，自定义进度条
+    plugins: [
+        progressPlugin,
+        new HtmlWebpackPlugin({template: './public/index.html', title: "first webpack"}),
+    ],
 };
