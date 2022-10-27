925e0c842a8e93b5d06daf1b62cd35b1c225fac8
杜培义 <peiyi.du@zknow.com>
2022-10-27 17:48:00 +0800

    [add]webpack的splitChunks，切割模块：优化node_modules编译携带到其他文件中的包的体积

--- src/print.js
+++ src/print.js
@@ -1,3 +1,6 @@
+import _ from 'loadsh'
+
 export default function printMe() {
     console.log('I get called from print.js!');
+    console.log(_.join(['Another', 'module', 'loaded!'], ' '));
 }
--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,23 @@ module.exports = {
     target: 'web', // 打包之后的运行， node和web
     // 入口文件
     entry: {
-        index: './src/index.js',
-        print: './src/print.js'
+        index: {
+            import: './src/index.js',
+            // 防止重复，共享
+            // dependOn: 'shared'
+        },
+        print: {
+            import: './src/print.js',
+            // 防止重复，共享
+            // dependOn: 'shared'
+        },
+        // 共享的工具库
+        // shared: 'lodash',
     },
     // 出口文件
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js', // name占位符来确保名字是唯一的
+        filename: '[name].[contenthash].js', // name占位符来确保名字是唯一的
         clean: true, // 每次build之前都出执行清除输出文件的操作
         // 公共的服务目录
         publicPath: "/",
@@ -42,7 +52,17 @@ module.exports = {
     },
     // 优化
     optimization: {
-        runtimeChunk: 'single'
+        runtimeChunk: 'single',
+        // 切割块
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: "all"
+                }
+            },
+        }
     },
     module: {
         rules: [
@@ -103,6 +123,6 @@ module.exports = {
     // ProgressPlugin进度插件，自定义进度条
     plugins: [
         progressPlugin,
-        new HtmlWebpackPlugin({template: './public/index.html', title: "first webpack"}),
+        new HtmlWebpackPlugin({template: './public/index.html', title: "Caching"}),
     ],
 };
